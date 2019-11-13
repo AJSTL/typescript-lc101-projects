8f3288bb8515270932ed08e84b09d2f2fcb485ef
AJ Meyer <ashleymeyer@ashleys-air.attlocal.net>
2019-11-13 11:35:44 -0600

    part4 completed

--- exercises/part4.js
+++ exercises/part4.js
@@ -1,42 +1,32 @@
-let kilometersToMars: number = 225000000;
-let kilometersToTheMoon: number = 384400;
-
-
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
 // // The variables that are commented out will be moved into the Spacecraft class
 // let spaceCraft: string = "Space Shuttle";
 // let speedMph: number = 17500;
 // let milesPerKilometer: number = 0.621;
-
 // // This function will also be moved into the Spacecraft class
 // function getDaysToLocation(kilometersAway: number): number {
 //   let milesAway: number = kilometersAway * milesPerKilometer;
 //   let hours: number = milesAway / speedMph;
 //   return hours / 24;
 // }
-
 // Define your Spacecraft class here:
-class Spacecraft {
-    milesPerKilometer: number = 0.621;
-    name: string;
-    speedMph: number;
-
-    constructor(name, speedMph) {
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(name, speedMph) {
+        this.milesPerKilometer = 0.621;
         this.name = name;
         this.speedMph = speedMph;
     }
-
-    getDaysToLocation(kilometersAway: number): number {
-        let milesAway: number = kilometersAway * this.milesPerKilometer;
-        let hours: number = milesAway / this.speedMph;
+    Spacecraft.prototype.getDaysToLocation = function (kilometersAway) {
+        var milesAway = kilometersAway * this.milesPerKilometer;
+        var hours = milesAway / this.speedMph;
         return hours / 24;
-    }
-}
-
+    };
+    return Spacecraft;
+}());
 // Create an instance of the class here:
-let spaceShuttle = new Spacecraft('Determination', 17500);
+var spaceShuttle = new Spacecraft('Determination', 17500);
 //console.log(spaceShuttle);
-
 // Print two outputs - one for the trip to Mars and one for the trip to the moon.
-
-console.log(`${spaceShuttle.name} takes ${spaceShuttle.getDaysToLocation(kilometersToMars)} days to get to Mars.`);
-console.log(`${spaceShuttle.name} takes ${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} days to get to the Moon.`);
+console.log(spaceShuttle.name + " takes " + spaceShuttle.getDaysToLocation(kilometersToMars) + " days to get to Mars.");
+console.log(spaceShuttle.name + " takes " + spaceShuttle.getDaysToLocation(kilometersToTheMoon) + " days to get to the Moon.");
